26d11be62e9e236fdc9b971d11bcc9ff7a08159a
Gregory Dutton <greg.dutton@gmail.com>
2023-05-20 15:35:51 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-// document.addEventListener('DOMContentLoaded', e => {
-//     document.addEventListener('submit', submitData(e));
-// };
-
 function submitData(name, email) {
-    const postObject = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: JSON.stringify({
-          name: `${name}`,
-          email: `${email}`,
-        }),
-      };
-    // console.log(postObject);
-    return fetch('http://localhost:3000/users', postObject)
-    .then(response => response.json());
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email: `${email}`,
+    }),
+  })
+  .then(response => response.json())
+  .then((data) => {
+    let info = document.querySelector("div");
+    info.id = data.id;
+  })
+  .catch(function (error) {
+    let info = document.querySelector("div");
+    info.id = error.message;
+  });
 }
-
-function fetchData(resource) {
-    
-    return fetch(`http://localhost:3000/dogs`)
-    .then(resp => resp.json())
-    .then(json => renderData(json))
-  }
-
-  function renderData(data) {
-    // console.log(data);
-    const main = document.querySelector('div');
-    data.forEach(item => {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = item.dogName;
-      main.appendChild(h2);
-    });
-  }
